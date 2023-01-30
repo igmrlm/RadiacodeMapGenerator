d06c9bc39d8f40c1ff54c8e82a174c94c82b7422
igmrlm <igmrlm@gmail.com>
2023-01-30 18:37:07 -0500

    Update MapGen.py

--- MapGen.py
+++ MapGen.py
@@ -1,19 +1,37 @@
 import folium
 import pandas as pd
+import argparse
 from folium.plugins import HeatMap
+import webbrowser
+
+
+# Define argparse for options
+parser = argparse.ArgumentParser()
+parser.add_argument('file', type=str, help='Input file name')
+parser.add_argument('--zoom', type=int, help='Map zoom level', default=15)
+parser.add_argument('--blur', type=int, help='Heatmap blur level', default=20)
+parser.add_argument('--opacity', type=float, help='Heatmap opacity', default=0.01)
+args = parser.parse_args()
+
+# Print out settings for run
+
+print(f"Now generating map from:\t{args.file}\nHeatMap blur level:\t\t{args.blur}\nHeatmap opacity:\t\t{args.opacity}\nMap zoom level:\t\t\t{args.zoom}")
 
 
 # Load data from file
-data = pd.read_csv('2023-01-22 Walking.rctrk', skiprows=1, delimiter='\t')
+data = pd.read_csv(args.file, skiprows=1, delimiter='\t')
 
 # Create map object
-m = folium.Map(location=[data.Latitude.mean(), data.Longitude.mean()], zoom_start=15)
+m = folium.Map(location=[data.Latitude.mean(), data.Longitude.mean()], zoom_start=args.zoom)
 
 # Create heatmap overlay
-HeatMap(data[['Latitude', 'Longitude', 'DoseRate']], overlay=True, name='Dose Rate', 
-        control=True, show=True, blur=20, opacity=0.01).add_to(m)
+HeatMap(data[['Latitude', 'Longitude', 'DoseRate']], overlay=True, name='Dose Rate',
+control=True, show=True, blur=args.blur, opacity=args.opacity).add_to(m)
 
 # Add layer control to toggle heatmap overlay
 folium.LayerControl().add_to(m)
 
 m.save('map.html')
+
+# Open in the System default webbrowser
+webbrowser.open('map.html')
